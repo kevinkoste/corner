d845026dde043657363fc5e3e577f53f9df32d68
Kevin Koste <kevin.koste@yale.edu>
2020-11-28 21:49:53 -0800

    full defaults

--- corner-api/src/app.ts
+++ corner-api/src/app.ts
@@ -29,12 +29,12 @@ app.use(
     resave: false,
     saveUninitialized: true,
     // unset: 'destroy',
-    cookie: {
-      // maxAge: 1000 * 60 * 60 * 24 * 7 * 2, // 2 weeks
-      secure: process.env.NODE_ENV === 'production',
-      // httpOnly: process.env.NODE_ENV !== 'production',
-      // sameSite: process.env.NODE_ENV === 'production' ? 'strict' : false,
-    },
+    // cookie: {
+    // maxAge: 1000 * 60 * 60 * 24 * 7 * 2, // 2 weeks
+    // secure: process.env.NODE_ENV === 'production',
+    // httpOnly: process.env.NODE_ENV !== 'production',
+    // sameSite: process.env.NODE_ENV === 'production' ? 'strict' : false,
+    // },
   })
 )
 
